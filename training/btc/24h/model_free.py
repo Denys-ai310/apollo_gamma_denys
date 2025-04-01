6f6df24f9acd1462c67cefc11e1d8e1a11b41d29
denys <dormaza973@gmail.com>
2025-04-02 05:35:55 +0900

    test free file

--- training/btc/24h/model_free.py
+++ training/btc/24h/model_free.py
@@ -3,9 +3,7 @@ import os
 import sys
 
 from stable_baselines3 import PPO, TD3, DDPG,  SAC, A2C
-# Add project root to Python path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
-from app.learners import learner_drl
+from learners import learner_drl
 from app.utils import paths, Dataset, utils
 from apollo_gamma.drl_envs.env_ld_sw_dataset import EnvTrading
 import matplotlib.pyplot as plt
